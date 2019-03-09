44f8736b429722af440ed356c3f1dcbbc252f34f
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-03-09 01:25:23 +0000

    tweak run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 python ./run.py --broadcast-title ${BROADCAST_TITLE}-$(date '+%Y-%m-%dT%H:%M:%S') --stream-title ${BROADCAST_TITLE}-$(date '+%Y-%m-%dT%H:%M:%S')
-ffmpeg -f lavfi \
+ffmpeg \
+  -f lavfi \
   -i anullsrc \
   -rtsp_transport udp \
   -i ${BROADCAST_URL} \
   -vcodec mpeg4 \
   -pix_fmt + \
-  -c:v copy \                                                 
+  -c:v copy \
   -f flv rtmp://a.rtmp.youtube.com/live2/$(cat /tmp/streamname.txt)
