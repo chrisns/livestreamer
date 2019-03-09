69e67e55e1cf583a7de4943635d0ff28a3f34e18
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-03-09 00:43:53 +0000

    add run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+python ./run.py --broadcast-title ${BROADCAST_TITLE}-$(date '+%Y-%m-%dT%H:%M:%S') --stream-title ${BROADCAST_TITLE}-$(date '+%Y-%m-%dT%H:%M:%S')
+ffmpeg -f lavfi \
+  -i anullsrc \
+  -i ${BROADCAST_URL} \
+  -vcodec mpeg4 \
+  -pix_fmt + \
+  -c:v libx264 \
+  -f flv rtmp://a.rtmp.youtube.com/live2/$(cat /tmp/streamname.txt)
