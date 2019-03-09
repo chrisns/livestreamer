3942ea6e1d20cb36d4357077dac67602079b689e
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-03-09 00:59:54 +0000

    udp traffic

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ set -e
 python ./run.py --broadcast-title ${BROADCAST_TITLE}-$(date '+%Y-%m-%dT%H:%M:%S') --stream-title ${BROADCAST_TITLE}-$(date '+%Y-%m-%dT%H:%M:%S')
 ffmpeg -f lavfi \
   -i anullsrc \
+  -rtsp_transport udp \
   -i ${BROADCAST_URL} \
   -vcodec mpeg4 \
   -pix_fmt + \
